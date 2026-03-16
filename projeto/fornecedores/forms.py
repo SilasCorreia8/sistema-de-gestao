232243deee5ae5e086622aa9ed1e4373481734d9
Silas Correia <silas8_silva@hotmail.com>
2026-03-16 16:47:39 -0300

    style: forms/fornecedor

--- projeto/fornecedores/forms.py
+++ projeto/fornecedores/forms.py
@@ -1,7 +1,16 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput
 from .models import Fornecedor
 
 class FornecedorForm(ModelForm):
     class Meta:
         model = Fornecedor
         fields = ["nome", "email", "telefone", "cidade", "estado", "cnpj"]
+
+        widgets = {
+            'nome': TextInput(attrs={'placeholder': 'Nome da Empresa / Fornecedor'}),
+            'cnpj': TextInput(attrs={'placeholder': 'CNPJ'}),
+            'email': EmailInput(attrs={'placeholder': 'E-mail de Contato'}),
+            'telefone': TextInput(attrs={'placeholder': 'Telefone'}),
+            'cidade': TextInput(attrs={'placeholder': 'Cidade'}),
+            'estado': TextInput(attrs={'placeholder': 'Estado'}),
+        }
